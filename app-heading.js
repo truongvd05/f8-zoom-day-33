a46ea574accef1e12e568eaeeabd5bd9519f9d00
truongvd05 <truongvdf8@fullstack.edu.vn>
2025-07-20 17:19:15 +0700

    add more modal

--- app-heading.js
+++ app-heading.js
@@ -3,6 +3,9 @@ class AppHeading extends HTMLElement {
         super();
         this.attachShadow({ mode: "open" });
     }
+    connectedCallback() {
+        this.hidden = false;
+    }
     open() {
         const template = document.querySelector("#template");
         const templateContent = template.content.cloneNode(true);
@@ -12,8 +15,12 @@ class AppHeading extends HTMLElement {
         const modal = this.shadowRoot.querySelector(".modal");
         modal.classList.add("fade-in");
         modal.classList.remove("fade-out");
+
         this.handleClose = (e) => {
-            if (e.target.closest(".close") || e.target.closest(".modal")) {
+            if (
+                e.target.closest(".close") ||
+                e.target.classList.contains("modal")
+            ) {
                 this.close();
             }
         };
@@ -22,11 +29,12 @@ class AppHeading extends HTMLElement {
             new CustomEvent("open", { bubbles: true, composed: true })
         );
     }
-
     close() {
         const modal = this.shadowRoot.querySelector(".modal");
-        modal.classList.remove("fade-in");
-        modal.classList.add("fade-out");
+        if (modal) {
+            modal.classList.remove("fade-in");
+            modal.classList.add("fade-out");
+        }
         modal.addEventListener("animationend", () => {
             this.shadowRoot.innerHTML = "";
         });
@@ -37,4 +45,4 @@ class AppHeading extends HTMLElement {
     }
 }
 
-customElements.define("app-heading", AppHeading);
+customElements.define("app-card", AppHeading);
--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 import "./app-heading.js";
 
 const btn = document.querySelector(".btn");
-const appModal = document.querySelector("app-heading");
-console.dir(appModal);
+const btn1 = document.querySelector(".btn1");
+const appModal1 = document.querySelector("#card1");
+const appModal2 = document.querySelector("#card2");
 
 btn.addEventListener("click", function (e) {
     e.preventDefault();
-    appModal.open();
+    appModal1.open();
+});
+
+btn1.addEventListener("click", function (e) {
+    e.preventDefault();
+    appModal2.open();
 });
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" || e.key === "x") {
-        appModal.close();
+        appModal1.close();
     }
 });
-
-appModal.addEventListener("open", () => {
-    console.log("Modal opened!");
-});
-
-appModal.addEventListener("close", () => {
-    console.log("Modal closed!");
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "x") {
+        appModal2.close();
+    }
 });
